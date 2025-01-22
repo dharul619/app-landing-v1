a3c6aef42f44240398773e8770fe46ca1669dcbf
dharul <dharulyuswansyah619@gmail.com>
2025-01-22 23:26:32 +0700

    final Commit

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
-var swiper = new Swiper(".image-slider", {
-  effect: "coverflow",
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: "auto",
-  coverflowEffect: {
-    rotate: 0,
-    stretch: 0,
-    depth: 100,
-    modifier: 2,
-    slideShadows: true,
-  },
-  loop: true,
-  autoplay: {
-    delay: 2000,
-    disableOnInteraction: false,
-  },
+document.addEventListener("DOMContentLoaded", function () {
+  var swiper = new Swiper(".image-slider", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 2,
+      slideShadows: true,
+    },
+    loop: true,
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
 });
-
-let menu = document.querySelector("#menu-bar");
-let navbar = document.querySelector(".navbar");
-
-menu.addEventListener("click", () => {
-  menu.classList.toggle("fa-times");
-  navbar.classList.toggle("nav-toggle");
-});
-
-window.onscroll = () => {
-  menu.classList.remove("fa-times");
-  navbar.classList.remove("nav-toggle");
-};
